3e0e5bbd62ea3c26548f8542aac40bc28951d208
Mathias Milling Hansen <331199@via.dk>
2025-04-07 13:09:01 +0200

    Added ferriswheel and jumpinggif animations to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,53 @@ export default function Home() {
                 className="object-cover image-rendering-pixelated"
                 quality={100}
             />
+            <div className="absolute top-135 left-335 transform -translate-x-1/2 -translate-y-1/2">
+            <Image
+                    src="/images/JumpingGifRepeat.gif"
+                    alt="Jumping Gif"
+                    width={50}
+                    height={50}
+                    className="animate-bounce"
+                    quality={100}
+                    style={{
+                        animationDuration: "1.5s", // Bounce duration
+                        animationDelay: "0.25s",  // Start at the bottom
+                        imageRendering: "pixelated", // Ensures sharp pixel art rendering
+                    }}
+
+                />
+                
+            </div>
+
+            <div className="absolute top-140 left-280 transform -translate-x-1/2 -translate-y-1/2">
+            <Image
+                    src="/images/JumpingGifRepeat.gif"
+                    alt="Jumping Gif"
+                    width={50}
+                    height={50}
+                    className="animate-bounce"
+                    quality={100}
+                    style={{
+                        animationDuration: "1.5s", // Bounce duration
+                        animationDelay: "0.25s",  // Start at the bottom
+                    }}
+                />
+                
+            </div>
+
+            <div className="absolute top-210 left-195 transform -translate-x-1/2 -translate-y-1/2">
+            <Image
+                    src="/images/FerrisWheel.gif"
+                    alt="Jumping Gif"
+                    width={200}
+                    height={200}
+                    quality={100}
+                    style={{
+                        imageRendering: "pixelated", // Ensures sharp pixel art rendering
+                    }}
+                />   
+            </div>     
+
         </div>
     );
 }
